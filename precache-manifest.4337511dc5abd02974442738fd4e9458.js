a7de1e5b19746a74622b1058f195199dc2047c6c
ashokr12 <rim.ashok@gmail.com>
2020-08-04 01:17:19 +0530

    Updates

---
+++ precache-manifest.4337511dc5abd02974442738fd4e9458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1b5bfbb6628c6944a86e9c7768e4b7",
+    "url": "/Cows_Bulls%20/index.html"
+  },
+  {
+    "revision": "2bcfdc48a2d4b8d81755",
+    "url": "/Cows_Bulls%20/static/css/main.cebaa50b.chunk.css"
+  },
+  {
+    "revision": "a6725a383d5564060d95",
+    "url": "/Cows_Bulls%20/static/js/2.e5d1bf3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cows_Bulls%20/static/js/2.e5d1bf3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bcfdc48a2d4b8d81755",
+    "url": "/Cows_Bulls%20/static/js/main.adee3a46.chunk.js"
+  },
+  {
+    "revision": "960383adb19980867da4",
+    "url": "/Cows_Bulls%20/static/js/runtime-main.0b573289.js"
+  }
+]);
